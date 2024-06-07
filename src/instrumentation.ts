4dfbe44dcd9e2d8efeee288bd94487e62b376922
RichardBray <richardbray89@gmail.com>
2024-06-07 18:33:38 +0100

    chore: using bs for metrics :(

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,17 +1,15 @@
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { SEMRESATTRS_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
-const { Resource } = require('@opentelemetry/resources');
-const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { NodeSDK } = require("@opentelemetry/sdk-node");
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { SEMRESATTRS_SERVICE_NAME } = require("@opentelemetry/semantic-conventions");
+const { Resource } = require("@opentelemetry/resources");
+const { PrometheusExporter } = require("@opentelemetry/exporter-prometheus");
 
 const sdk = new NodeSDK({
-  traceExporter: new OTLPTraceExporter(),
   metricReader: new PrometheusExporter(),
   instrumentations: [getNodeAutoInstrumentations()],
   views: [],
   resource: new Resource({
-    [SEMRESATTRS_SERVICE_NAME]: 'dice-roll',
+    [SEMRESATTRS_SERVICE_NAME]: "dice-roll",
   }),
 });
 
